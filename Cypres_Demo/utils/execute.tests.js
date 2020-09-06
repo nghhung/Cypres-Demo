f17a2731fd2eba0aacc35cbc7264c45251949ff4
Nguyenhung193 <hung.nguyen@enouvo.com>
2020-09-06 14:47:55 +0700

    update

--- Cypres_Demo/utils/execute.tests.js
+++ Cypres_Demo/utils/execute.tests.js
@@ -2,7 +2,8 @@ const npm = require("npm");
 
 // npm.load(() => npm.run("cy:run"));
 npm.load(() => npm.run("cy:test"));
-npm.load(() => npm.run("run:postman:json"));
-npm.load(() => npm.run("run:postman:html"));
+npm.load(() => npm.run("report"));
+// npm.load(() => npm.run("run:postman:json"));
+// npm.load(() => npm.run("run:postman:html"));
 npm.load(() => npm.run("save:results:postman"));
 npm.load(() => npm.run("save:results:cypress"));
